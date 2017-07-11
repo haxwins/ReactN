332a3a25350fc7294349ffb9b14ad9b50d063a4c
haxwins <haxregist@gmail.com>
2017-07-12 00:04:28 +0200

    1

---
+++ app.js
@@ -0,0 +1,9 @@
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+};
+const element = <Welcome name="Sara" />;
+ReactDOM.render(element,document.getElementById('roott'));
+var a=<h1>hello world xd!</h1>;
+var c=<h3>aasd</h3>;
+ReactDOM.render(a,document.getElementById('root'));
+ReactDOM.render(c,document.getElementById('ele'));

0aea3941d42155bb959c838e3d283582f0872266
haxwins <haxregist@gmail.com>
2017-07-21 01:42:17 +0200

    1

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+const colorArray = [
+	"red",
+	"pink",
+	"blue",
+	"yellow",
+	"green",
+	"cyan",
+	"orange",
+	"purple",
+	"hotpink",
+	"lightblue"
+];
+const textList = [
+	"JUST KIDDIN LMAO",
+	"ALRIGHT I'LL STOP NOW"
+];
+let pos = 0;
+let clickCounter = 0;	
+class ColorFull extends React.Component{
+	constructor(props){
+		super(props)
+		this.triggersBoth = this.triggersBoth.bind(this)
+		this.state = {
+			color: "black",
+			textcolor: "white",
+			text: "CLICK HERE TO CURE EPILEPSY"
+		}
+	}
+	render(){
+		const style = {
+		backgroundColor: this.state.color,
+		color: this.state.textcolor
+		};
+		return (
+				<div style={style} onMouseMove={this.change.bind(this)} onClick={this.triggersBoth.bind(this)}>
+					<p>{this.state.text}</p>
+				</div>
+				);
+	}
+	change(){
+		console.log(pos);
+		if(pos===true){
+			this.setState({color: "black"});
+			this.setState({textcolor: "white"});
+		}
+		else if(pos<colorArray.length-1){
+			this.setState({color: colorArray[pos]});
+			this.setState({textcolor: colorArray[pos+2]});
+			pos++;
+		}
+		else{
+			this.setState({color: colorArray[pos]});
+			pos=0;
+		}
+	}
+	click(){
+		clickCounter++;
+		if(clickCounter > 5){
+			pos = true;
+			this.setState({text: textList[1]});
+		} 
+		else
+			this.setState({text: textList[0]});
+	}
+		triggersBoth(){
+		this.change();
+		this.click();
+	};
 };
-const element = <Welcome name="Sara" />;
-ReactDOM.render(element,document.getElementById('roott'));
-var a=<h1>hello world xd!</h1>;
-var c=<h3>aasd</h3>;
-ReactDOM.render(a,document.getElementById('root'));
-ReactDOM.render(c,document.getElementById('ele'));
+ReactDOM.render(<ColorFull/>,document.getElementById('colorFull'));
